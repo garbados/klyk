abb01249516e6485f5dc18c5074146fdc732c917
garbados <garbados@gmail.com>
2012-11-17 14:12:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebestwaytomeetpeople::Application.config.secret_token = '225dfa018f7408676d69b4e61cacb3339051bec5c2e402b9f8812ed15ca343e692b84f2e77da6990c0e68f711cf126e4c2b143c4817dbc38012b271f7dc28233'
